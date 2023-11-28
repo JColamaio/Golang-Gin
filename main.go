401c5bddb0510dd676767ba75192041fb438d225
Jonathan Colamaio <jcolamaio@Jonathans-MacBook-Pro.local>
2023-11-28 05:58:16 -0300

    ground

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
-	"blog/cmd"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	cmd.Execute()
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run()
 }
